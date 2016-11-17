d18dafbb6cd7abb4d38c4f68843e81ac2f23dedf
james <imjamespond@gmai.com>
2016-11-18 00:47:38 +0800

    rpc support

---
+++ docs/rpc-service/src/main/java/com/james/App.java
@@ -0,0 +1,13 @@
+package com.james;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
